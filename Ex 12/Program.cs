5104bfbeb28934c1792e8acb084bc2d13493570d
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2024-01-02 20:52:52 +0200

    Add files via upload

---
+++ Ex 12/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Ex_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 64, 25, 12, 22, 11 };
+
+            Console.WriteLine("Array inainte de sortare:");
+            PrintArray(array);
+
+            SelectionSortFunction(array);
+
+            Console.WriteLine("\nArray dupa sortare:");
+            PrintArray(array);
+        }
+
+        static void SelectionSortFunction(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
