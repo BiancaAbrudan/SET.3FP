fd1ffd6fd1e2f5962059fa5659c4b5f53e855bb4
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2024-01-02 20:51:55 +0200

    Add files via upload

---
+++ Ex 11/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Ex_11
+{
+    internal class Program
+    {
+      
+        
+            static void AfiseazaPrimePanaLaN(int n)
+            {
+                bool[] estePrim = new bool[n + 1];
+
+                for (int i = 2; i <= n; i++)
+                {
+                    estePrim[i] = true;
+                }
+
+                for (int i = 2; i * i <= n; i++)
+                {
+                    if (estePrim[i])
+                    {
+                        for (int j = i * i; j <= n; j += i)
+                        {
+                            estePrim[j] = false;
+                        }
+                    }
+                }
+
+                Console.WriteLine($"Numere prime pana la {n}:");
+                for (int i = 2; i <= n; i++)
+                {
+                    if (estePrim[i])
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            static void Main()
+            {
+                Console.Write("Introduceti un numar natural n: ");
+                int n = int.Parse(Console.ReadLine());
+
+                AfiseazaPrimePanaLaN(n);
+            }
+        }
+    }
