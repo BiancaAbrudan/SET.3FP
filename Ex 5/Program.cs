6527a1e05761c6e3c0ff7b69c15738c43bf7b1b9
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2024-01-02 20:44:18 +0200

    Add files via upload

---
+++ Ex 5/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Ex_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea vectorului: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] vector = new int[n];
+
+            Console.WriteLine("Introduceti elementele vectorului:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Elementul {i}: ");
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Introduceti valoarea de inserat: ");
+            int e = int.Parse(Console.ReadLine());
+
+            Console.Write("Introduceti pozitia de inserat (index): ");
+            int k = int.Parse(Console.ReadLine());
+
+            if (k < 0 || k > n)
+            {
+                Console.WriteLine("Pozitia introdusa nu este valida.");
+            }
+            else
+            {
+                Array.Resize(ref vector, n + 1);
+
+                for (int i = n; i > k; i--)
+                {
+                    vector[i] = vector[i - 1];
+                }
+
+                vector[k] = e;
+
+                Console.WriteLine("\nVectorul cu valoarea inserata:");
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    Console.WriteLine($"Elementul {i}: {vector[i]}");
+                }
+            }
+        }
+    }
+}
