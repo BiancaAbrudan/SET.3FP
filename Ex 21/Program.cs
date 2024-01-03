9f3e00a60fc7fdde28477fcc601aeecfbf02c4e1
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2024-01-03 10:06:55 +0200

    Add files via upload

---
+++ Ex 21/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector1 = { 1, 2, 3, 4 };
+            int[] vector2 = { 1, 2, 3, 5 };
+
+            string ordineLexicografica = DeterminaOrdineLexicografica(vector1, vector2);
+
+            Console.WriteLine($"Ordinea lexicografica a vectorilor este: {ordineLexicografica}");
+        }
+
+        static string DeterminaOrdineLexicografica(int[] vector1, int[] vector2)
+        {
+            string str1 = ConvertVectorToString(vector1);
+            string str2 = ConvertVectorToString(vector2);
+
+            return str1.CompareTo(str2) < 0 ? "Primul vector" : "Al doilea vector";
+        }
+
+        static string ConvertVectorToString(int[] vector)
+        {
+            return string.Join(",", vector);
+        }
+    }
+}
