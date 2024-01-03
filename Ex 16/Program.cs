05488de68ac4b5a7ca43b28ce17d9554bc1d5fe9
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2024-01-03 10:02:21 +0200

    Add files via upload

---
+++ Ex 16/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ex_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 12, 18, 24 };
+
+            Console.WriteLine("Cel mai mare divizor comun al elementelor vectorului este: " + FindGCD(array));
+        }
+
+        static int FindGCD(int[] arr)
+        {
+            int result = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                result = GetGCD(result, arr[i]);
+
+                if (result == 1)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        static int GetGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return a;
+        }
+    }
+}
