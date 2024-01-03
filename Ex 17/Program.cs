1bbac71c04277eab53839738123a3f55fe5c957e
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2024-01-03 10:03:08 +0200

    Add files via upload

---
+++ Ex 17/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ex_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti numarul in baza 10: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Introduceti baza dorita (1 < b < 17): ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (b > 1 && b < 17)
+            {
+                string result = ConvertToBase(n, b);
+                Console.WriteLine($"Numarul {n} in baza {b} este: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Baza introdusa nu este valida. Baza trebuie sa fie intre 2 si 16.");
+            }
+        }
+
+        static string ConvertToBase(int num, int targetBase)
+        {
+            if (num == 0)
+            {
+                return "0";
+            }
+
+            string result = "";
+            while (num > 0)
+            {
+                int remainder = num % targetBase;
+                char digit = (char)(remainder < 10 ? '0' + remainder : 'A' + remainder - 10);
+                result = digit + result;
+                num /= targetBase;
+            }
+
+            return result;
+        }
+    }
+}
