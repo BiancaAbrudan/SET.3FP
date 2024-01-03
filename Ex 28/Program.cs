b554c5f8fc05a9893dd593d1491734ff67b867d2
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2024-01-03 10:12:23 +0200

    Add files via upload

---
+++ Ex 28/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Ex_28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+
+            Console.WriteLine("Vectorul initial: " + string.Join(", ", vector));
+
+            QuickSort(vector, 0, vector.Length - 1);
+
+            Console.WriteLine("Vectorul sortat: " + string.Join(", ", vector));
+        }
+
+        static void QuickSort(int[] vector, int stanga, int dreapta)
+        {
+            if (stanga < dreapta)
+            {
+                int pivotIndex = Partitioneaza(vector, stanga, dreapta);
+
+                QuickSort(vector, stanga, pivotIndex - 1);
+                QuickSort(vector, pivotIndex + 1, dreapta);
+            }
+        }
+
+        static int Partitioneaza(int[] vector, int stanga, int dreapta)
+        {
+            int pivot = vector[dreapta];
+            int i = stanga - 1;
+
+            for (int j = stanga; j < dreapta; j++)
+            {
+                if (vector[j] < pivot)
+                {
+                    i++;
+
+                    int temp = vector[i];
+                    vector[i] = vector[j];
+                    vector[j] = temp;
+                }
+            }
+
+            int tempSwap = vector[i + 1];
+            vector[i + 1] = vector[dreapta];
+            vector[dreapta] = tempSwap;
+
+            return i + 1;
+        }
+    }
+}
